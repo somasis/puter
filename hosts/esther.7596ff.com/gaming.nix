890ecd686d546ac243aa22126cf268588a0798c7
Kylie McClain <kylie@somas.is>
2025-05-21 17:08:39 -0400

    gaming: fix Steam library permissions :(

--- hosts/esther.7596ff.com/gaming.nix
+++ hosts/esther.7596ff.com/gaming.nix
@@ -1,17 +1,28 @@
+{ lib
+, config
+, ...
+}:
 let
   steamLibrary = "/var/lib/steam";
 in
-{
+lib.optionalAttrs config.programs.steam.enable {
   persist.directories = [{
-    mode = "3775";
-    user = "nobody";
-    group = "users";
+    mode = "6775";
+    user = "root";
+    group = "root";
     directory = steamLibrary;
   }];
 
   systemd.tmpfiles.rules = [
-    "A ${steamLibrary} - - - - default:user:cassie:rwX"
-    "A ${steamLibrary} - - - - default:user:somasis:rwX"
-    "A ${steamLibrary} - - - - default:other::rw"
+    "A ${steamLibrary} - - - - user::rwx"
+    "A ${steamLibrary} - - - - group::r-x"
+    "A ${steamLibrary} - - - - group:users:rwx"
+    "A ${steamLibrary} - - - - mask::rwx"
+    "A ${steamLibrary} - - - - other::r-x"
+    "A ${steamLibrary} - - - - default:user::rwx"
+    "A ${steamLibrary} - - - - default:group::r-x"
+    "A ${steamLibrary} - - - - default:group:users:rwx"
+    "A ${steamLibrary} - - - - default:mask::rwx"
+    "A ${steamLibrary} - - - - default:other::r-x"
   ];
 }
