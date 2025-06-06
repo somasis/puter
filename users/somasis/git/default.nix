e3c9dbc7256b05b8995684907e99432068cfa082
Kylie McClain <kylie@somas.is>
2025-06-06 03:54:39 -0400

    git: clean up, fix signing

--- users/somasis/git/default.nix
+++ users/somasis/git/default.nix
@@ -9,139 +9,225 @@
     ./signing.nix
   ];
 
-  programs.git = {
-    enable = true;
-    package = pkgs.gitFull;
-
-    userName = "Kylie McClain";
-    userEmail = "kylie@somas.is";
-
-    aliases = {
-      addall = "add -Av";
-      addp = "add -p";
-      unadd = "reset HEAD --";
-
-      com = "commit";
-      amend = "commit --amend";
-      amendall = "!git addall; >/dev/null EDITOR=cat git amend";
-
-      commits = "log --reverse --oneline @{upstream}...HEAD";
-      patches = "format-patch --stdout origin..HEAD";
-
-      re = "rebase";
-      ri = "rebase -i";
-      rbe = "rebase --edit-todo";
-      rbc = "rebase --continue";
-      rbs = "rebase --skip";
-      rba = "rebase --abort";
-
-      ch = "cherry-pick";
-      chc = "cherry-pick --continue";
-      chs = "cherry-pick --skip";
-      cha = "cherry-pick --abort";
-    };
-
-    difftastic = {
-      enable = true;
-      display = "inline";
-    };
-
-    extraConfig = {
-      sendemail = {
-        annotate = true;
-        smtpserver = lib.getExe (config.programs.msmtp.package or pkgs.msmtp);
-      };
-
-      init.defaultBranch = "main";
-      interactive.singlekey = true;
-
-      pull.rebase = true;
-      push = {
-        default = "simple";
-        rebase = true;
-
-        autoSetupRemote = true;
-      };
-
-      fetch = {
-        output = "compact";
-
-        writeCommitGraph = true; # can help with performance
-
-        # prune = true;
-        # pruneTags = true;
-      };
-
-      log.abbrevCommit = false;
-
-      branch = {
-        autoSetupMerge = "simple";
-        autoSetupRebase = "always";
-      };
-
-      # Sort branch and tag lists by the date they were modified/created.
-      branch.sort = "-committerdate";
-      tag.sort = "taggerdate";
-
-      diff = {
-        # Detect renames more aggressively.
-        renames = "copies";
-
-        # Detects moved chunks of lines better than the default algorithm.
-        algorithm = "histogram";
-
-        # Don't print a/ and b/ prefixes on diffs in `git log`.
-        noprefix = true;
-      };
-
-      commit = {
-        # Show a diff at the end of the commit message during editing.
-        verbose = true;
-
-        # Keep lines that start with comment indicators before the scissors line.
-        cleanup = "scissors";
-      };
-
-      stash.showPatch = true;
-      status = {
-        showStash = true; # --show-stash
-        branch = true; # -b, --branch
-        short = true; # -s, --short
-      };
-
-      # Prefer origin's branches when switching without specifying a remote.
-      checkout.defaultRemote = "origin";
-
-      # Parallelize more things.
-      checkout.workers = "-1";
-      fetch.parallel = "0";
-      submodule.fetchJobs = "0";
-
-      # <https://github.com/NixOS/nixpkgs/issues/169193#issuecomment-1116090241>
-      safe.directory = "*";
-
-      url = {
-        "github:".insteadOf = "ssh://git@github.com:";
-        "git://github.com/".insteadOf = "ssh://git@github.com:";
-        "https://github.com/".insteadOf = "ssh://git@github.com:";
-
-        "gitlab:".insteadOf = "ssh://git@gitlab.com:";
-        "git://gitlab.com/".insteadOf = "ssh://git@gitlab.com:";
-        "https://gitlab.com/".insteadOf = "ssh://git@gitlab.com:";
-
-        "sourcehut:".insteadOf = "ssh://git@git.sr.ht:";
-        "git://git.sr.ht/".insteadOf = "ssh://git@git.sr.ht:";
-        "https://git.sr.ht/".insteadOf = "ssh://git@git.sr.ht:";
-      };
-    };
-  };
-
   cache.directories = [
     { directory = config.lib.somasis.xdgCacheDir "pre-commit"; method = "symlink"; }
+    { directory = config.lib.somasis.xdgCacheDir "treefmt"; method = "symlink"; }
   ];
 
-  home.shellAliases =
-    {
+  programs = {
+    git = {
+      enable = true;
+      package = pkgs.gitFull;
+
+      userName = "Kylie McClain";
+      userEmail = "kylie@somas.is";
+
+      aliases = {
+        addall = "add -Av";
+        addp = "add -p";
+        unadd = "reset HEAD --";
+
+        com = "commit";
+        amend = "commit --amend";
+        amendall = "!git addall; >/dev/null EDITOR=cat git amend";
+
+        commits = "log --reverse --oneline @{upstream}...HEAD";
+        patches = "format-patch --stdout origin..HEAD";
+
+        re = "rebase";
+        ri = "rebase -i";
+        rbe = "rebase --edit-todo";
+        rbc = "rebase --continue";
+        rbs = "rebase --skip";
+        rba = "rebase --abort";
+
+        ch = "cherry-pick";
+        chc = "cherry-pick --continue";
+        chs = "cherry-pick --skip";
+        cha = "cherry-pick --abort";
+      };
+
+      difftastic = {
+        enable = true;
+        display = "inline";
+      };
+
+      extraConfig = {
+        sendemail = {
+          annotate = true;
+          smtpserver = lib.getExe (config.programs.msmtp.package or pkgs.msmtp);
+        };
+
+        init.defaultBranch = "main";
+        interactive.singlekey = true;
+
+        pull.rebase = true;
+        push = {
+          default = "simple";
+          rebase = true;
+
+          autoSetupRemote = true;
+        };
+
+        fetch = {
+          output = "compact";
+
+          writeCommitGraph = true; # can help with performance
+
+          # prune = true;
+          # pruneTags = true;
+        };
+
+        log.abbrevCommit = false;
+
+        branch = {
+          autoSetupMerge = "simple";
+          autoSetupRebase = "always";
+        };
+
+        # Sort branch and tag lists by the date they were modified/created.
+        branch.sort = "-committerdate";
+        tag.sort = "taggerdate";
+
+        diff = {
+          # Detect renames more aggressively.
+          renames = "copies";
+
+          # Detects moved chunks of lines better than the default algorithm.
+          algorithm = "histogram";
+
+          # Don't print a/ and b/ prefixes on diffs in `git log`.
+          noprefix = true;
+        };
+
+        commit = {
+          # Show a diff at the end of the commit message during editing.
+          verbose = true;
+
+          # Keep lines that start with comment indicators before the scissors line.
+          cleanup = "scissors";
+        };
+
+        stash.showPatch = true;
+        status = {
+          showStash = true; # --show-stash
+          branch = true; # -b, --branch
+          short = true; # -s, --short
+        };
+
+        # Prefer origin's branches when switching without specifying a remote.
+        checkout.defaultRemote = "origin";
+
+        # Parallelize more things.
+        checkout.workers = "-1";
+        fetch.parallel = "0";
+        submodule.fetchJobs = "0";
+
+        # <https://github.com/NixOS/nixpkgs/issues/169193#issuecomment-1116090241>
+        safe.directory = "*";
+
+        url = {
+          "github:".insteadOf = "ssh://git@github.com:";
+          "git://github.com/".insteadOf = "ssh://git@github.com:";
+          "https://github.com/".insteadOf = "ssh://git@github.com:";
+
+          "gitlab:".insteadOf = "ssh://git@gitlab.com:";
+          "git://gitlab.com/".insteadOf = "ssh://git@gitlab.com:";
+          "https://gitlab.com/".insteadOf = "ssh://git@gitlab.com:";
+
+          "sourcehut:".insteadOf = "ssh://git@git.sr.ht:";
+          "git://git.sr.ht/".insteadOf = "ssh://git@git.sr.ht:";
+          "https://git.sr.ht/".insteadOf = "ssh://git@git.sr.ht:";
+        };
+      };
+    };
+
+    bash.initExtra = ''
+      _git_prompt() {
+          [ -n "''${_git_prompt:=$(git rev-parse --abbrev-ref=loose HEAD 2>/dev/null)}" ] \
+              && printf '%s ' "''${_git_prompt}"
+          _git_prompt=
+      }
+
+      gitlukin() {
+          set -- $(
+              git log \
+                  --color=always \
+                  --no-merges \
+                  --oneline \
+                  --reverse "$@" \
+              | sk \
+                  --ansi \
+                  --no-sort \
+                  -d ' ' \
+                  --preview='git log --color=always -1 --patch-with-stat {1}' \
+                  --preview-window=down:75% \
+              | cut -d' ' -f1
+          )
+          log --no-merges "$@"
+      }
+    '';
+
+    kakoune.config.hooks = [
+      # Show git diff on save
+      {
+        name = "BufCreate";
+        option = ".*";
+        commands = ''
+          evaluate-commands %sh{ git rev-parse >/dev/null 2>&1 && echo git show-diff || : }
+        '';
+      }
+      {
+        name = "BufWritePost";
+        option = ".*";
+        commands = ''
+          evaluate-commands %sh{ git rev-parse >/dev/null 2>&1 && echo git show-diff || : }
+        '';
+      }
+
+      # Lightly enforce the 50/72 rule for git(1) commit summaries.
+      {
+        name = "WinSetOption";
+        option = "filetype=git-commit";
+        commands = ''
+          # Commit title; everything over 50 is yellow.
+          add-highlighter window/ regex \A\n*[^#\n]{50}([^\n]+) 1:black,yellow+f
+
+          # Line following the title should be empty.
+          add-highlighter window/ regex \A[^\n]*\n([^#\n]+) 1:white,red+b
+        '';
+      }
+
+      # Wrap git commits to 72.
+      {
+        name = "WinSetOption";
+        option = "filetype=git-.*";
+        commands = ''set-option window autowrap_column 72'';
+      }
+    ];
+  };
+
+  home = {
+    packages = [
+      pkgs.pre-commit
+
+      pkgs.git-open
+
+      (pkgs.writeShellScriptBin "git-curlam" ''
+        set -e
+
+        b=$(git rev-parse HEAD)
+
+        ${pkgs.curl}/bin/curl -Lf# "$@" \
+            | ${config.programs.git.package}/bin/git am -q
+
+        a=$(git rev-parse HEAD)
+
+        git log --oneline --reverse "$b".."$a"
+      '')
+    ];
+
+    shellAliases = {
       am = "git am";
       add = "git add -v";
 
@@ -169,100 +255,8 @@
     }
     # Add git aliases to the shell
     // lib.mapAttrs
-      (
-        _: v: if lib.hasPrefix "!" v then lib.removePrefix "!" v else "git ${v}"
-      )
-      config.programs.git.aliases;
-
-  programs.bash.initExtra = ''
-    _git_prompt() {
-        [ -n "''${_git_prompt:=$(git rev-parse --abbrev-ref=loose HEAD 2>/dev/null)}" ] \
-            && printf '%s ' "''${_git_prompt}"
-        _git_prompt=
-    }
-
-    gitlukin() {
-        set -- $(
-            git log \
-                --color=always \
-                --no-merges \
-                --oneline \
-                --reverse "$@" \
-            | sk \
-                --ansi \
-                --no-sort \
-                -d ' ' \
-                --preview='git log --color=always -1 --patch-with-stat {1}' \
-                --preview-window=down:75% \
-            | cut -d' ' -f1
-        )
-        log --no-merges "$@"
-    }
-  '';
-
-  programs.kakoune.config.hooks = [
-    # Show git diff on save
-    {
-      name = "BufCreate";
-      option = ".*";
-      commands = "evaluate-commands %sh{ git rev-parse >/dev/null 2>&1 && echo git show-diff || : }";
-    }
-    {
-      name = "BufWritePost";
-      option = ".*";
-      commands = "evaluate-commands %sh{ git rev-parse >/dev/null 2>&1 && echo git show-diff || : }";
-    }
-
-    # Lightly enforce the 50/72 rule for git(1) commit summaries.
-    {
-      name = "WinSetOption";
-      option = "filetype=git-commit";
-      commands = ''
-        # Commit title; everything over 50 is yellow.
-        add-highlighter window/ regex \A\n*[^#\n]{50}([^\n]+) 1:black,yellow+f
-
-        # Line following the title should be empty.
-        add-highlighter window/ regex \A[^\n]*\n([^#\n]+) 1:white,red+b
-      '';
-    }
-
-    # Wrap git commits to 72.
-    {
-      name = "WinSetOption";
-      option = "filetype=git-.*";
-      commands = ''set-option window autowrap_column 72'';
-    }
-
-    # # Honor pre-commit files.
-    # {
-    #   name = "BufWritePre";
-    #   option = ".*";
-    #   commands = ''
-    #     evaluate-commands %sh{
-    #         if [ -e "$(git rev-parse --show-toplevel 2>/dev/null || echo /dev/null)"/.pre-commit-config.yaml ]; then
-    #             printf 'set-option window formatcmd "%s"\n' 'pre-commit run --files %val{buffile}'
-    #         fi
-    #     }
-    #   '';
-    # }
-  ];
-
-  home.packages = [
-    pkgs.pre-commit
-
-    pkgs.git-open
-
-    (pkgs.writeShellScriptBin "git-curlam" ''
-      set -e
-
-      b=$(git rev-parse HEAD)
-
-      ${pkgs.curl}/bin/curl -Lf# "$@" \
-          | ${config.programs.git.package}/bin/git am -q
-
-      a=$(git rev-parse HEAD)
-
-      git log --oneline --reverse "$b".."$a"
-    '')
-  ];
+      (_: v: if lib.hasPrefix "!" v then lib.removePrefix "!" v else "git ${v}")
+      config.programs.git.aliases
+    ;
+  };
 }
--- users/somasis/git/signing.nix
+++ users/somasis/git/signing.nix
@@ -11,13 +11,15 @@
 
     # Sign all commits and tags by default.
     signing.signByDefault = true;
-    signing.key = null;
+    # signing.key = null;
     # It is unnecessary to set `signing.key`, because git-config says
     # > user.signingKey
     # >     [...] If not set Git will call gpg.ssh.defaultKeyCommand
     # >     (e.g.: "ssh-add -L") and try to use the first key available.
     # which means it'll just use the first available key in the agent.
-    extraConfig.gpg.ssh.defaultKeyCommand = "ssh-add -L";
+    # FIXME yeah but what if you're not really using ssh-agent?
+    # can we have that key be loaded into ssh-agent automatically instead?
+    signing.key = "~/.ssh/id_ed25519";
 
     # Store trusted signatures.
     extraConfig.gpg.ssh.allowedSignersFile = "~/.ssh/allowed_signers";
