325355a2cdde82852cf6dc00a820b21f46973197
Kylie McClain <kylie@somas.is>
2025-06-06 20:54:53 -0400

    sensible-defaults: fix rescue mode

--- modules/nixos/sensible-defaults/boot.nix
+++ modules/nixos/sensible-defaults/boot.nix
@@ -1,4 +1,9 @@
-{ config, pkgs, ... }:
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
 {
   # Allow for avoiding usage of mountpoint=legacy for the root zpool.
   boot.zfs = {
@@ -14,21 +19,26 @@
     "/persist".neededForBoot = lib.mkDefault true;
   };
 
-  # HACK Required for booting to work with disko's configuration!
-  # See <https://github.com/nix-community/disko/issues/312#issuecomment-1666187472>.
-  boot.initrd.systemd.services.rollback = {
-    description = "Rollback ZFS datasets to a pristine state";
-    serviceConfig.Type = "oneshot";
-    unitConfig.DefaultDependencies = "no";
-    wantedBy = [ "initrd.target" ];
-    after = [ "zfs-import-${config.networking.fqdnOrHostName}.service" ];
-    before = [ "sysroot.mount" ];
-    path = with pkgs; [
-      zfs
-    ];
-    script = ''
-      set -ex
-      zfs rollback -r ${config.networking.fqdnOrHostName}/nixos/root/runtime@blank && echo "rollback complete"
-    '';
+  boot.initrd.systemd = {
+    # Allow logging in at root when rescue mode happens during initrd.
+    # The system is encryptyed anyway so it's not really that much a security risk.
+    services.rescue.environment.ENABLE_FORCED_SULOGIN = 1;
+    # HACK Required for booting to work with disko's configuration!
+    # See <https://github.com/nix-community/disko/issues/312#issuecomment-1666187472>.
+    services.rollback = {
+      description = "Rollback ZFS datasets to a pristine state";
+      serviceConfig.Type = "oneshot";
+      unitConfig.DefaultDependencies = "no";
+      wantedBy = [ "initrd.target" ];
+      after = [ "zfs-import-${config.networking.fqdnOrHostName}.service" ];
+      before = [ "sysroot.mount" ];
+      path = with pkgs; [
+        zfs
+      ];
+      script = ''
+        set -ex
+        zfs rollback -r ${config.networking.fqdnOrHostName}/nixos/root/runtime@blank && echo "rollback complete"
+      '';
+    };
   };
 }
