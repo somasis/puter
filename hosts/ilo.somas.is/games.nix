60533720d8060f3079adbcad1f42c7e2eccd0a80
Kylie McClain <kylie@somas.is>
2025-06-17 00:41:42 -0400

    Revert "Trim gaming configuration"
    This reverts commit 697a1b152827736dbd42c10d71ba9b1a5488cf93.

--- hosts/ilo.somas.is/games.nix
+++ hosts/ilo.somas.is/games.nix
@@ -4,17 +4,29 @@
   ...
 }:
 {
-  programs = {
-    gamemode = {
-      enable = true;
+  programs.gamemode = {
+    enable = true;
 
-      settings.general = {
+    settings = {
+      general = {
         renice = "19";
         inhibit_screensaver = "0";
       };
-    };
 
-    steam.enable = true;
+      custom = {
+        start = "${pkgs.systemd}/bin/systemctl --user start game.target";
+        stop = "${pkgs.systemd}/bin/systemctl --user stop game.target";
+      };
+    };
+  };
+
+  programs.steam.enable = true;
+
+  systemd.user.targets.game = {
+    description = "Gaming mode (ensure system utilizes lower resources than normal)";
+
+    after = [ "gamemoded.service" ];
+    wants = [ "gamemoded.service" ];
   };
 
   # RetroArch joysticks and stuff
--- users/somasis/desktop/games/default.nix
+++ users/somasis/desktop/games/default.nix
@@ -1,5 +1,6 @@
 {
   config,
+  lib,
   osConfig,
   pkgs,
   ...
@@ -42,65 +43,101 @@
 
       itch
 
-      pcsx2 # Sony - PlayStation 2
-      (
-        assert (osConfig.hardware.graphics.enable);
-        assert (osConfig.services.udev.enable && osConfig.hardware.uinput.enable);
-        pkgs.retroarch.override (prev: {
-          cores = with pkgs.libretro; [
-            stella # Atari - 2600
-            virtualjaguar # Atari - Jaguar
-            prboom # DOOM
-            # mame # MAME
-            freeintv # Mattel - Intellivision
-            mgba # Nintendo - Game Boy Advance
-            sameboy # Nintendo - Game Boy / Nintendo - Game Boy Color
-            dolphin # Nintendo - GameCube / Nintendo - Wii
-            citra # Nintendo - Nintendo 3DS
-            mupen64plus # Nintendo - Nintendo 64
-            parallel-n64 # Nintendo - Nintendo 64 (Dr. Mario 64)
-            melonds # Nintendo - Nintendo DS
-            mesen # Nintendo - Nintendo Entertainment System / Nintendo - Family Computer Disk System
-            snes9x # Nintendo - Super Nintendo Entertainment System
-            picodrive # Sega - 32X
-            flycast # Sega - Dreamcast
-            genesis-plus-gx # Sega - Mega-Drive - Genesis
-            beetle-saturn # Sega - Saturn
-            swanstation # Sony - PlayStation
-            ppsspp # Sony - PlayStation Portable
-          ];
-        })
-      )
+      lutris
+
+      # (
+      #   assert (osConfig.hardware.graphics.enable);
+      #   assert (osConfig.services.udev.enable && osConfig.hardware.uinput.enable);
+      #   pkgs.retroarch.override (prev: {
+      #     cores = with pkgs.libretro; [
+      #       stella # Atari - 2600
+      #       virtualjaguar # Atari - Jaguar
+      #       prboom # DOOM
+      #       # mame # MAME
+      #       freeintv # Mattel - Intellivision
+      #       mgba # Nintendo - Game Boy Advance
+      #       sameboy # Nintendo - Game Boy / Nintendo - Game Boy Color
+      #       dolphin # Nintendo - GameCube / Nintendo - Wii
+      #       citra # Nintendo - Nintendo 3DS
+      #       mupen64plus # Nintendo - Nintendo 64
+      #       parallel-n64 # Nintendo - Nintendo 64 (Dr. Mario 64)
+      #       melonds # Nintendo - Nintendo DS
+      #       mesen # Nintendo - Nintendo Entertainment System / Nintendo - Family Computer Disk System
+      #       snes9x # Nintendo - Super Nintendo Entertainment System
+      #       picodrive # Sega - 32X
+      #       flycast # Sega - Dreamcast
+      #       genesis-plus-gx # Sega - Mega-Drive - Genesis
+      #       beetle-saturn # Sega - Saturn
+      #       swanstation # Sony - PlayStation
+      #       pcsx2 # Sony - PlayStation 2
+      #       ppsspp # Sony - PlayStation Portable
+      #     ];
+
+      #     # settings = prev.settings // settingsRender settings;
+      #   })
+      # )
     ];
 
-  persist.directories = [
+  home.shellAliases = lib.mkIf osConfig.programs.gamemode.enable {
+    pcsx2 = "gamemoderun pcsx2-qt -fullscreen -bigpicture";
+  };
+
+  cache.directories = [
     {
       method = "symlink";
-      directory = config.lib.somasis.xdgConfigDir "retroarch";
+      directory = config.lib.somasis.xdgCacheDir "lutris";
     }
+
     {
       method = "symlink";
-      directory = config.lib.somasis.xdgConfigDir "PCSX2";
-    }
-    {
-      method = "symlink";
-      directory = config.lib.somasis.xdgConfigDir "opentyrian";
-    }
-    {
-      method = "symlink";
-      directory = config.lib.somasis.xdgConfigDir "tetrio-desktop";
-    }
-    {
-      method = "symlink";
-      directory = config.lib.somasis.xdgDataDir "SpaceCadetPinball";
-    }
-    {
-      method = "symlink";
-      directory = ".lbreakouthd";
-    }
-    {
-      method = "symlink";
-      directory = ".zaz";
+      directory = config.lib.somasis.xdgCacheDir "retroarch";
     }
   ];
+
+  sync = {
+    directories = [
+      {
+        method = "symlink";
+        directory = config.lib.somasis.xdgDataDir "lutris";
+      }
+      {
+        method = "symlink";
+        directory = config.lib.somasis.xdgDataDir "games";
+      } # used for ROM files as well
+
+      {
+        method = "symlink";
+        directory = config.lib.somasis.xdgConfigDir "retroarch";
+      }
+      {
+        method = "symlink";
+        directory = config.lib.somasis.xdgDataDir "retroarch";
+      }
+      {
+        method = "symlink";
+        directory = config.lib.somasis.xdgConfigDir "PCSX2";
+      }
+
+      {
+        method = "symlink";
+        directory = config.lib.somasis.xdgConfigDir "opentyrian";
+      }
+      {
+        method = "symlink";
+        directory = config.lib.somasis.xdgConfigDir "tetrio-desktop";
+      }
+      {
+        method = "symlink";
+        directory = config.lib.somasis.xdgDataDir "SpaceCadetPinball";
+      }
+      {
+        method = "symlink";
+        directory = ".lbreakouthd";
+      }
+      {
+        method = "symlink";
+        directory = ".zaz";
+      }
+    ];
+  };
 }
