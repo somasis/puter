d960f3680fb359097e90451e9dca27f7f6c15eee
Kylie McClain <kylie@somas.is>
2025-06-17 00:42:19 -0400

    Trim gaming configuration (correctly this time)

--- users/somasis/desktop/games/default.nix
+++ users/somasis/desktop/games/default.nix
@@ -1,6 +1,5 @@
 {
   config,
-  lib,
   osConfig,
   pkgs,
   ...
@@ -43,101 +42,62 @@
 
       itch
 
-      lutris
-
-      # (
-      #   assert (osConfig.hardware.graphics.enable);
-      #   assert (osConfig.services.udev.enable && osConfig.hardware.uinput.enable);
-      #   pkgs.retroarch.override (prev: {
-      #     cores = with pkgs.libretro; [
-      #       stella # Atari - 2600
-      #       virtualjaguar # Atari - Jaguar
-      #       prboom # DOOM
-      #       # mame # MAME
-      #       freeintv # Mattel - Intellivision
-      #       mgba # Nintendo - Game Boy Advance
-      #       sameboy # Nintendo - Game Boy / Nintendo - Game Boy Color
-      #       dolphin # Nintendo - GameCube / Nintendo - Wii
-      #       citra # Nintendo - Nintendo 3DS
-      #       mupen64plus # Nintendo - Nintendo 64
-      #       parallel-n64 # Nintendo - Nintendo 64 (Dr. Mario 64)
-      #       melonds # Nintendo - Nintendo DS
-      #       mesen # Nintendo - Nintendo Entertainment System / Nintendo - Family Computer Disk System
-      #       snes9x # Nintendo - Super Nintendo Entertainment System
-      #       picodrive # Sega - 32X
-      #       flycast # Sega - Dreamcast
-      #       genesis-plus-gx # Sega - Mega-Drive - Genesis
-      #       beetle-saturn # Sega - Saturn
-      #       swanstation # Sony - PlayStation
-      #       pcsx2 # Sony - PlayStation 2
-      #       ppsspp # Sony - PlayStation Portable
-      #     ];
-
-      #     # settings = prev.settings // settingsRender settings;
-      #   })
-      # )
+      pcsx2 # Sony - PlayStation 2
+      (
+        assert (osConfig.hardware.graphics.enable);
+        assert (osConfig.services.udev.enable && osConfig.hardware.uinput.enable);
+        retroarch.withCores (
+          cores: with cores; [
+            stella # Atari - 2600
+            # mame # MAME
+            mgba # Nintendo - Game Boy Advance
+            sameboy # Nintendo - Game Boy / Nintendo - Game Boy Color
+            dolphin # Nintendo - GameCube / Nintendo - Wii
+            citra # Nintendo - Nintendo 3DS
+            mupen64plus # Nintendo - Nintendo 64
+            parallel-n64 # Nintendo - Nintendo 64 (Dr. Mario 64)
+            melonds # Nintendo - Nintendo DS
+            mesen # Nintendo - Nintendo Entertainment System / Nintendo - Family Computer Disk System
+            snes9x # Nintendo - Super Nintendo Entertainment System
+            picodrive # Sega - 32X
+            flycast # Sega - Dreamcast
+            genesis-plus-gx # Sega - Mega-Drive - Genesis
+            beetle-saturn # Sega - Saturn
+            swanstation # Sony - PlayStation
+            ppsspp # Sony - PlayStation Portable
+          ]
+        )
+      )
     ];
 
-  home.shellAliases = lib.mkIf osConfig.programs.gamemode.enable {
-    pcsx2 = "gamemoderun pcsx2-qt -fullscreen -bigpicture";
-  };
-
-  cache.directories = [
+  persist.directories = [
     {
       method = "symlink";
-      directory = config.lib.somasis.xdgCacheDir "lutris";
+      directory = config.lib.somasis.xdgConfigDir "retroarch";
     }
-
     {
       method = "symlink";
-      directory = config.lib.somasis.xdgCacheDir "retroarch";
+      directory = config.lib.somasis.xdgConfigDir "PCSX2";
+    }
+    {
+      method = "symlink";
+      directory = config.lib.somasis.xdgConfigDir "opentyrian";
+    }
+    {
+      method = "symlink";
+      directory = config.lib.somasis.xdgConfigDir "tetrio-desktop";
+    }
+    {
+      method = "symlink";
+      directory = config.lib.somasis.xdgDataDir "SpaceCadetPinball";
+    }
+    {
+      method = "symlink";
+      directory = ".lbreakouthd";
+    }
+    {
+      method = "symlink";
+      directory = ".zaz";
     }
   ];
-
-  sync = {
-    directories = [
-      {
-        method = "symlink";
-        directory = config.lib.somasis.xdgDataDir "lutris";
-      }
-      {
-        method = "symlink";
-        directory = config.lib.somasis.xdgDataDir "games";
-      } # used for ROM files as well
-
-      {
-        method = "symlink";
-        directory = config.lib.somasis.xdgConfigDir "retroarch";
-      }
-      {
-        method = "symlink";
-        directory = config.lib.somasis.xdgDataDir "retroarch";
-      }
-      {
-        method = "symlink";
-        directory = config.lib.somasis.xdgConfigDir "PCSX2";
-      }
-
-      {
-        method = "symlink";
-        directory = config.lib.somasis.xdgConfigDir "opentyrian";
-      }
-      {
-        method = "symlink";
-        directory = config.lib.somasis.xdgConfigDir "tetrio-desktop";
-      }
-      {
-        method = "symlink";
-        directory = config.lib.somasis.xdgDataDir "SpaceCadetPinball";
-      }
-      {
-        method = "symlink";
-        directory = ".lbreakouthd";
-      }
-      {
-        method = "symlink";
-        directory = ".zaz";
-      }
-    ];
-  };
 }
