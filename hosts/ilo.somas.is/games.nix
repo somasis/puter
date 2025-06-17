697a1b152827736dbd42c10d71ba9b1a5488cf93
Kylie McClain <kylie@somas.is>
2025-06-17 00:39:17 -0400

    Trim gaming configuration

--- hosts/ilo.somas.is/games.nix
+++ hosts/ilo.somas.is/games.nix
@@ -4,29 +4,17 @@
   ...
 }:
 {
-  programs.gamemode = {
-    enable = true;
+  programs = {
+    gamemode = {
+      enable = true;
 
-    settings = {
-      general = {
+      settings.general = {
         renice = "19";
         inhibit_screensaver = "0";
       };
-
-      custom = {
-        start = "${pkgs.systemd}/bin/systemctl --user start game.target";
-        stop = "${pkgs.systemd}/bin/systemctl --user stop game.target";
-      };
     };
-  };
 
-  programs.steam.enable = true;
-
-  systemd.user.targets.game = {
-    description = "Gaming mode (ensure system utilizes lower resources than normal)";
-
-    after = [ "gamemoded.service" ];
-    wants = [ "gamemoded.service" ];
+    steam.enable = true;
   };
 
   # RetroArch joysticks and stuff
