8635bff907e0005e387770ae64ea650414519f0d
Kylie McClain <kylie@somas.is>
2025-07-15 00:41:42 -0400

    theme: don't set windowDecorations and splashScreen
    As suggested by the warning given by plasma-manager,
    when setting them in addition to `programs.plasma.workspace.lookAndFeel`.

--- users/somasis/theme/default.nix
+++ users/somasis/theme/default.nix
@@ -18,16 +18,8 @@ in
   programs.plasma = {
     workspace = {
       lookAndFeel = "org.kde.breezetwilight.desktop";
-
       theme = "breeze-dark";
       colorScheme = "SomasisLight";
-
-      windowDecorations = {
-        library = "org.kde.breeze";
-        theme = "Breeze";
-      };
-
-      splashScreen.theme = "None";
     };
 
     configFile.kdeglobals.General.AccentColor = config.lib.somasis.colors.kde config.theme.colors.accent;
@@ -38,24 +30,20 @@ in
 
     lightModeScripts = {
       kde-color-scheme = ''
-        set -x
         plasma-apply-colorscheme SomasisLight
         plasma-apply-colorscheme -a ${lib.escapeShellArg (colors.hex config.theme.colors.accent)}
       '';
       kde-gtk-theme = ''
-        set -x
         dbus-send --session --dest=org.kde.GtkConfig --type=method_call /GtkConfig org.kde.GtkConfig.setGtkTheme "string:Breeze-gtk"
       '';
     };
 
     darkModeScripts = {
       kde-color-scheme = ''
-        set -x
         plasma-apply-colorscheme SomasisDark
         plasma-apply-colorscheme -a ${lib.escapeShellArg (colors.hex config.theme.colors.brightAccent)}
       '';
       kde-gtk-theme = ''
-        set -x
         dbus-send --session --dest=org.kde.GtkConfig --type=method_call /GtkConfig org.kde.GtkConfig.setGtkTheme "string:Breeze-dark-gtk"
       '';
     };
