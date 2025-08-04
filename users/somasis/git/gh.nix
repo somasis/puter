d11f03f9763dd520700b65ee87db46f337c33f81
Kylie McClain <kylie@somas.is>
2025-08-04 18:03:09 -0400

    Revert "gh: remove use of wrapCommand"
    This reverts commit 925eeb6534f369d04707a6eb1fa31901ef7d0d88.
    This commit broke `gh` by requiring that `gh auth login` be
    ran before the first use after boot; wrapCommand can be fixed
    to avoid using the deprecated stuff it's using.

--- users/somasis/git/gh.nix
+++ users/somasis/git/gh.nix
@@ -18,6 +18,23 @@
       extensions = [
         pkgs.gh-eco
       ];
+
+      package = pkgs.wrapCommand {
+        package = pkgs.gh;
+
+        wrappers = [
+          {
+            setEnvironmentDefault.GH_HOST = "github.com";
+            beforeCommand = [
+              ''
+                set +x
+                : "''${GH_TOKEN:=$(${config.programs.password-store.package}/bin/pass "gh/$GH_HOST/''${USER:-$(id -un)}")}"
+                export GH_TOKEN
+              ''
+            ];
+          }
+        ];
+      };
     };
 
     gh-dash.enable = true;
