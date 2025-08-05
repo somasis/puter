e187c82fdf9ed7f8ce6c83cc78c4f5692eaf46dc
Kylie McClain <kylie@somas.is>
2025-08-04 23:32:59 -0400

    syncthing: remove dead code

--- users/somasis/desktop/syncthing.nix
+++ users/somasis/desktop/syncthing.nix
@@ -11,7 +11,6 @@ let
   seed = host: "${config.home.username}@${host}";
 
   syncthingGuiPortFor = host: randomPort ''${seed host}:syncthing-gui'';
-  syncthingListenPortFor = host: randomPort ''${seed host}:syncthing-listen'';
 
   syncthingGuiPort = syncthingGuiPortFor osConfig.networking.fqdnOrHostName;
 in
