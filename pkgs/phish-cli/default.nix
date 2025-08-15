b00436cafd13b8c7505ee0f2d984edea0452d118
Kylie McClain <kylie@somas.is>
2025-08-15 00:38:35 -0400

    phish-cli: initial package

---
+++ pkgs/phish-cli/default.nix
@@ -0,0 +1,56 @@
+{
+  lib,
+  symlinkJoin,
+  writeShellApplication,
+  pkgs,
+}:
+let
+  bins = [
+    ../../bin/beet-import-phish
+    ../../bin/phishin-auth-login
+    ../../bin/phishin-download-show
+    ../../bin/phishin-like-show
+    ../../bin/phishin-list-shows
+    ../../bin/phish-show-notes
+  ];
+
+  mans = [ ];
+
+  inherit (builtins) readFile;
+  inherit (lib)
+    pipe
+    splitString
+    filter
+    concatStrings
+    hasPrefix
+    replaceStrings
+    getAttrFromPath
+    ;
+in
+symlinkJoin {
+  name = "phish-cli";
+
+  paths = map (
+    textPath:
+    writeShellApplication {
+      name = baseNameOf textPath;
+      text = builtins.readFile textPath;
+
+      runtimeInputs = pipe (readFile textPath) [
+        (splitString "\n")
+        (x: filter (hasPrefix "#! nix-shell") x)
+        (x: concatStrings (map (replaceStrings [ ''#! nix-shell -i bash -p '' ] [ "" ]) x))
+        (splitString " ")
+        (map (pkgAttr: getAttrFromPath ([ "pkgs" ] ++ (splitString "." pkgAttr)) pkgs))
+      ];
+    }
+  ) bins;
+
+  postInstall = lib.optionalString (mans != [ ]) (lib.escapeShellArgs ([ "installManPage" ] ++ mans));
+
+  meta = with lib; {
+    licenses = licenses.cc0;
+    maintainers = [ maintainers.somasis ];
+    mainProgram = null;
+  };
+}
