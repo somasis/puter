98364e14dec8347a04c29fba6c1366b0d4418553
Kylie McClain <kylie@somas.is>
2025-09-15 00:44:59 -0400

    www: remove ~/www (~/src/www is used now)

--- users/somasis/desktop/www.nix
+++ users/somasis/desktop/www.nix
@@ -3,18 +3,11 @@
   ...
 }:
 {
-  persist.directories = [
-    {
-      method = "symlink";
-      directory = "www";
-    }
-  ];
-
   home.packages = with pkgs; [
     asciidoctor-with-extensions
     curlFull
     imagemagick
   ];
 
-  home.shellAliases.note = ''$EDITOR "$(make -C ~/www/somas.is -s note-new)"'';
+  home.shellAliases.note = ''$EDITOR "$(make -C ~/src/www/somas.is -s note-new)"'';
 }
