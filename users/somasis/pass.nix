22b3cae5b773666e612b2d29fb9a92a73fe29028
Kylie McClain <kylie@somas.is>
2025-09-24 16:31:11 -0400

    pass: remove `pass`; auto-unlock keepassxc

--- users/somasis/pass.nix
+++ users/somasis/pass.nix
@@ -38,23 +38,12 @@ let
 
 in
 {
-  persist = {
-    directories = [
-      (config.lib.somasis.xdgDataDir "password-store")
-      (config.lib.somasis.xdgConfigDir "keepassxc")
-    ];
-
-    files = [
-      (config.lib.somasis.xdgConfigDir "kleopatrarc")
-    ];
-  };
+  persist.directories = [
+    (config.lib.somasis.xdgConfigDir "keepassxc")
+  ];
 
   cache.directories = [
     (config.lib.somasis.xdgCacheDir "keepassxc")
-  ];
-
-  sync.directories = [
-    ".gnupg"
   ];
 
   home.packages =
@@ -62,42 +51,24 @@ in
     with kdePackages;
     [
       # keep-sorted start
-      gnupg
       keepassxc
-      kleopatra
       libsecret
-      # pass-secrets
-      pinentry-qt
       rofi # used by qute-keepassxc
       # keep-sorted end
     ];
 
-  programs.password-store = {
-    enable = true;
-
-    settings.PASSWORD_STORE_CLIP_TIME = builtins.toString 60;
-
-    package = pkgs.pass-wayland.withExtensions (
-      exts:
-      with exts;
-      with pkgs.passExtensions;
-      [
-        # keep-sorted start
-        pass-botp
-        pass-checkup
-        pass-meta
-        pass-otp
-        pass-update
-        # keep-sorted end
-      ]
-    );
-  };
-
-  # FIXME Provide libsecret service for various apps
-  # Fails with multiple GPG keys specified in .gpg-id, in part due
-  # to the fact that pass-secret-service isn't using `pass` directly.
-  # `pass-secrets` just uses `pass` which I think is better anyway.
-  # services.pass-secret-service.enable = true;
+  xdg.autostart.entries = [
+    # Start with database unlocked and window minimized.
+    (
+      pkgs.makeDesktopItem {
+        name = "keepassxc";
+        desktopName = "KeepassXC";
+        icon = "keepassxc";
+        exec = "/usr/bin/env keepassxc --minimized --keyfile ${keepassKeyFile} ${keepassDatabaseFile}";
+      }
+      + "/share/applications/keepassxc.desktop"
+    )
+  ];
 
   programs.qutebrowser = {
     aliases.keepassxc = "spawn -u qute-keepassxc --insecure";
@@ -107,16 +78,6 @@ in
       zL = "keepassxc --totp";
     };
   };
-
-  # NOTE Workaround <https://github.com/NixOS/nixpkgs/issues/183604>
-  programs.bash.initExtra =
-    let
-      completions = "${config.programs.password-store.package}/share/bash-completion/completions";
-    in
-    lib.mkAfter ''
-      source ${completions}/pass-*
-      source ${completions}/pass
-    '';
 
   home.sessionVariables = {
     # Used by `bin/phishin-auth-login`, among other things.
