8d86f3d4e37a0b4c4cdbaf1e4c75250d8b0577d5
Kylie McClain <kylie@somas.is>
2025-10-02 17:48:27 -0400

    users: remove dead code

--- hosts/ilo.somas.is/users.nix
+++ hosts/ilo.somas.is/users.nix
@@ -41,28 +41,9 @@
         # $ sudo zfs change-key ilo.somas.is/nixos
         # $ pass edit ilo.somas.is/zfs/nixos
         hashedPassword = "$6$1vjLB9lSU6Xw8J.L$8zmUO3J9dXUQfAIqIkCBroOpQ3KXUjBJsmu5NZrnO3IB1GyIqXpkUUgZP3XXCJ1./x9TK./06M4bnvYX/PYzs/";
-
-        openssh.authorizedKeys.keys = [
-          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPkmjWLpicEaQOkM7FAv5bctmZjV5GjISYW7re0oknLU somasis@ilo.somas.is_20220603"
-          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILwx+D9HPPjg0H6rSLUaXiEOQzF9W4LlX3HRgyD+4eis somasis@esther.7596ff.com_20250221"
-        ];
       };
     };
   };
 
   services.displayManager.autoLogin.user = "somasis";
-  # IDEA: Synchronize user passwords with password store?
-  # system.activationScripts.pass-users = {
-  #   text = ''
-  #     set -eu
-  #     set -o pipefail
-
-  #     for u in ${builtins.attrNames config.users.users}; do
-  #         if p=$(pass "${config.networking.fqdnOrHostName}/users/$u" 2>&1); then
-  #             printf '%s:%s\n' "$u" "$(tr -d '\n' <<< "$p" | mkpasswd -m sha-512 -s)"
-  #         fi
-  #     done
-  #     # chpasswd -e
-  #   '';
-  # };
 }
