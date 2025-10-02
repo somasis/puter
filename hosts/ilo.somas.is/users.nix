a4382aec12309690c140ca670fa9a86da6b060bd
Kylie McClain <kylie@somas.is>
2025-10-02 17:48:22 -0400

    users: keep sorted

--- hosts/ilo.somas.is/users.nix
+++ hosts/ilo.somas.is/users.nix
@@ -18,16 +18,18 @@
         extraGroups = [
           "systemd-journal"
         ]
+        # keep-sorted start
         ++ lib.optional config.hardware.brillo.enable "video"
-        ++ lib.optional config.virtualisation.podman.enable "podman"
         ++ lib.optional config.hardware.sane.enable "scanner"
         ++ lib.optional config.hardware.uinput.enable "input"
         ++ lib.optional config.programs.adb.enable "adbusers"
         ++ lib.optional config.programs.tcpdump.enable "pcap"
         ++ lib.optional config.security.sudo.enable "wheel"
+        ++ lib.optional config.security.tpm2.enable "tss"
         ++ lib.optional config.services.printing.enable "lp"
         ++ lib.optional config.services.timesyncd.enable "systemd-timesync"
-        ++ lib.optional config.security.tpm2.enable "tss"
+        ++ lib.optional config.virtualisation.podman.enable "podman"
+        # keep-sorted end
         ++ lib.optionals config.networking.networkmanager.enable [
           "network"
           "networkmanager"
