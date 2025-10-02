de3f7eb9a69d012849ea8a0ac3489b9b779b995b
Kylie McClain <kylie@somas.is>
2025-10-02 18:15:04 -0400

    users: make users mutable

--- hosts/ilo.somas.is/users.nix
+++ hosts/ilo.somas.is/users.nix
@@ -4,44 +4,39 @@
   ...
 }:
 {
+  persist.files = [
+    "/etc/passwd"
+    "/etc/shadow"
+    "/etc/group"
+  ];
+
   users = {
-    mutableUsers = false;
+    mutableUsers = true;
 
-    users = {
-      root.hashedPassword = "$y$j9T$RI9UZXWVwReRSKDI9qhyw/$atD3ojK0Wp4fSMkzZD7jxM.HR/Sp9uj9UsnA5mYRso7";
+    users.somasis = {
+      isNormalUser = true;
+      description = "Kylie McClain";
+      uid = 1000;
 
-      somasis = {
-        isNormalUser = true;
-        description = "Kylie McClain";
-        uid = 1000;
-
-        extraGroups = [
-          "systemd-journal"
-        ]
-        # keep-sorted start
-        ++ lib.optional config.hardware.brillo.enable "video"
-        ++ lib.optional config.hardware.sane.enable "scanner"
-        ++ lib.optional config.hardware.uinput.enable "input"
-        ++ lib.optional config.programs.adb.enable "adbusers"
-        ++ lib.optional config.programs.tcpdump.enable "pcap"
-        ++ lib.optional config.security.sudo.enable "wheel"
-        ++ lib.optional config.security.tpm2.enable "tss"
-        ++ lib.optional config.services.printing.enable "lp"
-        ++ lib.optional config.services.timesyncd.enable "systemd-timesync"
-        ++ lib.optional config.virtualisation.podman.enable "podman"
-        # keep-sorted end
-        ++ lib.optionals config.networking.networkmanager.enable [
-          "network"
-          "networkmanager"
-        ];
-
-        # $ mkpasswd -m sha-512 -s
-        # and don't forget...
-        # $ pass edit ilo.somas.is/users/somasis
-        # $ sudo zfs change-key ilo.somas.is/nixos
-        # $ pass edit ilo.somas.is/zfs/nixos
-        hashedPassword = "$6$1vjLB9lSU6Xw8J.L$8zmUO3J9dXUQfAIqIkCBroOpQ3KXUjBJsmu5NZrnO3IB1GyIqXpkUUgZP3XXCJ1./x9TK./06M4bnvYX/PYzs/";
-      };
+      extraGroups = [
+        "systemd-journal"
+      ]
+      # keep-sorted start
+      ++ lib.optional config.hardware.brillo.enable "video"
+      ++ lib.optional config.hardware.sane.enable "scanner"
+      ++ lib.optional config.hardware.uinput.enable "input"
+      ++ lib.optional config.programs.adb.enable "adbusers"
+      ++ lib.optional config.programs.tcpdump.enable "pcap"
+      ++ lib.optional config.security.sudo.enable "wheel"
+      ++ lib.optional config.security.tpm2.enable "tss"
+      ++ lib.optional config.services.printing.enable "lp"
+      ++ lib.optional config.services.timesyncd.enable "systemd-timesync"
+      ++ lib.optional config.virtualisation.podman.enable "podman"
+      # keep-sorted end
+      ++ lib.optionals config.networking.networkmanager.enable [
+        "network"
+        "networkmanager"
+      ];
     };
   };
 
