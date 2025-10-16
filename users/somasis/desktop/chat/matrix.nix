9c28cd18c72140413a194bfcedb59b039b16ef43
Kylie McClain <kylie@somas.is>
2025-10-15 20:40:01 -0400

    matrix: style

--- users/somasis/desktop/chat/matrix.nix
+++ users/somasis/desktop/chat/matrix.nix
@@ -13,19 +13,20 @@
   ];
 
   persist = {
-    files = [
-      (config.lib.somasis.xdgConfigDir "neochatrc")
-      (config.lib.somasis.xdgConfigDir "KDE/neochat.conf")
-      (config.lib.somasis.xdgConfigDir "kunifiedpush-org.kde.neochat")
+    directories = [
+      (config.lib.somasis.xdgCacheDir "KDE/neochat")
+      (config.lib.somasis.xdgDataDir "KDE/neochat")
     ];
 
-    directories = [
-      (config.lib.somasis.xdgDataDir "KDE/neochat")
+    files = [
+      (config.lib.somasis.xdgConfigDir "KDE/neochat.conf")
+      (config.lib.somasis.xdgConfigDir "kunifiedpush-org.kde.neochat")
+      (config.lib.somasis.xdgConfigDir "neochatrc")
     ];
   };
 
-  # Required to build packages like pkgs.kdePackages.neochat, due to its dependency
-  # on libolm, which is deprecated...
+  # Required to build packages like pkgs.kdePackages.neochat, due to its
+  # dependency on libolm, which is deprecated...
   nixpkgs.config.permittedInsecurePackages = [
     "olm-3.2.16"
   ];
