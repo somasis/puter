f71e515bd6a01e995b4b5a98ac2d5d2e40e55fd9
Kylie McClain <kylie@somas.is>
2025-10-20 23:24:23 -0400

    secrets: remove dead code, move secrets.nix to root

---
+++ secrets.nix
@@ -0,0 +1,44 @@
+# deadnix: skip
+# This file is *not* used by the NixOS configuration,
+# it is only used by the `agenix` command line tool.
+let
+  recipients = {
+    ilo = "age1tpm1qtfwxktsqmuw363sjalejf80qasph0dzawu38rs8h9ah5fqsc0zekwu5ev8";
+    somasis.ilo = "age1tpm1qd0gxkaj69v6s44khyfrdkqry4tvs3gm0rjl6m74fmys37q0dfk3szkxadd";
+  };
+
+  secrets = with recipients; {
+    # Before committing any modifications to the list of recipients for
+    # any file listed here, run `agenix -r` in the development environment.
+    "rclone-fastmail-pass.age".publicKeys = [ somasis.ilo ];
+    "rclone-nextcloud-pass.age".publicKeys = [ somasis.ilo ];
+    "rclone-whatbox-http-url.age".publicKeys = [ somasis.ilo ];
+    "rclone-whatbox-ilo-pass.age".publicKeys = [ somasis.ilo ];
+    "rclone-vault-password.age".publicKeys = [ somasis.ilo ];
+    "rclone-vault-password2.age".publicKeys = [ somasis.ilo ];
+
+    # Used by host, not by a specific user on the host.
+    "ntfy-token-ilo.somas.is.age".publicKeys = [
+      ilo
+      somasis.ilo
+    ];
+    "restic-ilo.somas.is.age".publicKeys = [
+      ilo
+      somasis.ilo
+    ];
+    "restic-rclone-whatbox.age".publicKeys = [
+      ilo
+      somasis.ilo
+    ];
+  };
+in
+builtins.listToAttrs (
+  builtins.attrValues (
+    builtins.mapAttrs (name: value: {
+      name = "secrets/${name}";
+      value = value // {
+        armor = true;
+      };
+    }) secrets
+  )
+)
