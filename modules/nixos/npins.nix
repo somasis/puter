1a33b3d1dda6b70a81dd5b5a723772224a52b6e0
Kylie McClain <kylie@somas.is>
2025-10-22 23:56:32 -0400

    nixosModules.npins: update
    - Ensure that the system's nixpkgs is available at /etc/npins/nixpkgs,
      and thus also as `<nixpkgs>` and `flake:nixpkgs`, consistent with
      the behavior of system-wide channels and Flakes.
    - Improve comments
    - Don't wrap everything in `config = { ... }`

--- modules/nixos/npins.nix
+++ modules/nixos/npins.nix
@@ -1,46 +1,46 @@
 {
   sources,
-  nixpkgs,
+  nixpkgs ? (throw "The nixpkgs npins source used by this configuration needs to be provided"),
   pkgs,
   lib ? pkgs.lib,
-  config,
   ...
 }:
+let
+  # Ensure the system's nixpkgs source is named "nixpkgs", so that
+  # <nixpkgs> and `nixpkgs#...` refer to the same thing that they
+  # usually do when using Flakes and channels system-wide.
+  sources' = sources // {
+    inherit nixpkgs;
+  };
+in
 {
-  config = {
-    environment = {
-      systemPackages = [ pkgs.npins ];
-      etc.npins.source = pkgs.linkFarm "npins-sources" (
-        lib.mapAttrsToList (name: src: {
-          inherit name;
-          path = src.outPath;
-        }) sources
-      );
-    };
+  environment = {
+    systemPackages = [ pkgs.npins ];
+    etc.npins.source = pkgs.linkFarm "npins-sources" (
+      lib.mapAttrsToList (name: src: {
+        inherit name;
+        path = src.outPath;
+      }) sources'
+    );
+  };
 
-    system.nixos.revision = nixpkgs.revision or (builtins.baseNameOf (builtins.dirOf nixpkgs.url));
+  system.nixos.revision = nixpkgs.revision or (builtins.baseNameOf (builtins.dirOf nixpkgs.url));
 
-    nix = {
-      # Disable all points of dependency pulling other than npins.
-      # channel.enable = false;
-      # registry = lib.mkForce {};
+  # Disable all points of dependency pulling other than npins.
+  nix = {
+    channel.enable = false;
 
-      # Use Flake registry to provide access to npins' source paths;
-      # and rather than putting those paths directly into NIX_PATH,
-      # have the NIX_PATH entries refer to the flake registry. This
-      # ensures that we don't have to make permanent paths to our
-      channel.enable = false;
-      nixPath = [
-        "nixos-config=${../../hosts/${config.networking.fqdnOrHostName}}"
-      ]
-      ++ (lib.mapAttrsToList (n: _: "${n}=/etc/npins/${n}") sources);
+    # Set $NIX_PATH to our sources in /etc/npins.
+    nixPath = lib.mkForce (lib.mapAttrsToList (n: _: "${n}=/etc/npins/${n}") sources');
 
-      registry = lib.mapAttrs (n: v: {
+    # Translate npins sources to Flakes in the system registry.
+    registry = lib.mkForce (
+      lib.mapAttrs (n: v: {
         to = {
           type = "path";
-          path = v;
+          path = v.outPath;
         };
-      }) sources;
-    };
+      }) sources'
+    );
   };
 }
