8a9b047f25dda9190d78488f72a8c894e10c4bce
Kylie McClain <kylie@somas.is>
2025-10-22 23:50:53 -0400

    chat/facebook: remove dead code

--- users/somasis/desktop/chat/facebook.nix
+++ users/somasis/desktop/chat/facebook.nix
@@ -1,9 +1,6 @@
 {
   config,
   pkgs,
-  lib,
-  inputs,
-  osConfig,
   ...
 }:
 let
@@ -31,53 +28,13 @@ let
   };
 
   caprineConfigFile = (pkgs.formats.json { }).generate "caprine-config.json" caprineConfig;
-
-  makeCssFontFamily = familyName: fontList: ''
-    @font-face {
-        font-family: "${familyName}";
-        src: ${
-          lib.pipe fontList [
-            (map (font: "local(\"${font}\")"))
-            (lib.concatStringsSep ",")
-          ]
-        };
-    }
-  '';
 in
 {
   home.packages = [ caprine ];
 
-  persist.directories = [ (config.lib.somasis.xdgConfigDir caprineWindowClassName) ];
+  persist.directories = [ (config.lib.somasis.xdgConfigDir "Caprine") ];
 
   systemd.user.tmpfiles.rules = [
     "C ${config.xdg.configHome}/${caprineWindowClassName}/config.json - - - 0 ${caprineConfigFile}"
   ];
-
-  xdg.configFile."${caprineWindowClassName}/custom.css".text = ''
-    ${makeCssFontFamily "system-ui" osConfig.fonts.fontconfig.defaultFonts.sansSerif}
-    ${makeCssFontFamily "-apple-system" osConfig.fonts.fontconfig.defaultFonts.sansSerif}
-    ${makeCssFontFamily "BlinkMacSystemFont" osConfig.fonts.fontconfig.defaultFonts.sansSerif}
-    ${makeCssFontFamily "emoji" osConfig.fonts.fontconfig.defaultFonts.emoji}
-    ${makeCssFontFamily "sans-serif" osConfig.fonts.fontconfig.defaultFonts.sansSerif}
-    ${makeCssFontFamily "serif" osConfig.fonts.fontconfig.defaultFonts.serif}
-    ${makeCssFontFamily "monospace" osConfig.fonts.fontconfig.defaultFonts.monospace}
-    ${makeCssFontFamily "ui-sans-serif" osConfig.fonts.fontconfig.defaultFonts.sansSerif}
-    ${makeCssFontFamily "ui-serif" osConfig.fonts.fontconfig.defaultFonts.serif}
-    ${makeCssFontFamily "ui-monospace" osConfig.fonts.fontconfig.defaultFonts.monospace}
-
-    body,
-    button,
-    input,
-    label,
-    select,
-    td,
-    textarea {
-        font-family: ui-sans-serif, sans-serif !important;
-    }
-
-    pre,
-    code {
-        font-family: ui-monospace, monospace !important;
-    }
-  '';
 }
