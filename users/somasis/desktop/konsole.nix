b0c7b329b8c674b06e69b804aa10c47c26fe343d
Kylie McClain <kylie@somas.is>
2025-10-24 15:30:27 -0400

    konsole: improve application profile

--- users/somasis/desktop/konsole.nix
+++ users/somasis/desktop/konsole.nix
@@ -3,6 +3,62 @@
   lib,
   ...
 }:
+let
+  baseProfile = {
+    colorScheme = "somasis";
+    font = {
+      name = config.programs.plasma.fonts.fixedWidth.family;
+      size = config.programs.plasma.fonts.fixedWidth.pointSize;
+    };
+
+    extraConfig = {
+      Appearance = {
+        LineSpacing = 0;
+        BoldIntense = true; # otherwise, nothing seems to even happen with bold fonts!
+      };
+
+      "Cursor Options".CursorShape = 1; # I-Beam
+
+      General = {
+        DimWhenInactive = false;
+        InvertSelectionColors = true;
+        SemanticInputClick = true;
+        SemanticUpDown = true;
+        TerminalCenter = true;
+        TerminalMargin = 2;
+        TerminalColumns = 90;
+      };
+
+      "Interaction Options" = {
+        AllowEscapedLinks = true;
+        AutoCopySelectedText = false;
+        EscapedLinksSchema = lib.concatStringsSep ";" [
+          "http://"
+          "https://"
+          "file://"
+          "gopher://"
+          "gemini://"
+          "ftp://"
+          "ftps://"
+          "ssh://"
+          "git://"
+        ];
+      };
+
+      Scrolling = {
+        HistoryMode = 1; # Limited history
+        HistorySize = 10000; # lines
+        MarkerColor = config.lib.somasis.colors.kde config.theme.colors.orange;
+        ScrollBarPosition = 2; # Disable scrollbar
+      };
+
+      "Terminal Features" = {
+        BlinkingCursorEnabled = true;
+        FlowControlEnabled = false;
+      };
+    };
+  };
+in
 {
   cache = with config.lib.somasis; {
     files = [
@@ -43,96 +99,44 @@
 
       defaultProfile = "somasis";
       customColorSchemes.somasis = ./konsole.colorscheme;
-      profiles = with lib; rec {
-        somasis = {
-          colorScheme = "somasis";
-          font = {
-            name = config.programs.plasma.fonts.fixedWidth.family;
-            size = config.programs.plasma.fonts.fixedWidth.pointSize;
-          };
+      profiles = with lib; {
+        somasis = baseProfile;
 
+        application = lib.recursiveUpdate baseProfile {
           extraConfig = {
-            Appearance = {
-              LineSpacing = 0;
-              BoldIntense = true; # otherwise, nothing seems to even happen with bold fonts!
-            };
-
-            "Cursor Options".CursorShape = 1; # I-Beam
-
             General = {
-              DimWhenInactive = false;
-              InvertSelectionColors = true;
-              SemanticInputClick = true;
-              SemanticUpDown = true;
-              TerminalCenter = true;
-              TerminalMargin = 2;
-              TerminalColumns = 90;
+              Environment = lib.concatStringsSep "," (
+                mapAttrsToList
+                  (
+                    n: v:
+                    assert isValidPosixName n;
+                    "${n}=${v}"
+                  )
+                  {
+                    TERM = "xterm-256color";
+                    COLORTERM = "truecolor";
+                  }
+              );
+
+              Icon = "window";
+
+              LocalTabTitleFormat = "%n";
             };
 
             "Interaction Options" = {
-              AllowEscapedLinks = true;
-              AutoCopySelectedText = false;
-              EscapedLinksSchema = concatStringsSep ";" [
-                "http://"
-                "https://"
-                "file://"
-                "gopher://"
-                "gemini://"
-                "ftp://"
-                "ftps://"
-                "ssh://"
-                "git://"
-              ];
+              ColorFilterEnabled = false;
+              CtrlRequiredForDrag = false;
+              MiddleClickPasteMode = 1;
+              OpenLinksByDirectClickEnabled = false;
+              UnderlineFilesEnabled = true;
             };
 
+            # Disable all scrolling related enhancements
             Scrolling = {
+              HighlightScrolledLines = false;
               HistoryMode = 1; # Limited history
-              HistorySize = 10000; # lines
-              MarkerColor = config.lib.somasis.colors.kde config.theme.colors.orange;
               ScrollBarPosition = 2; # Disable scrollbar
             };
-
-            "Terminal Features" = {
-              BlinkingCursorEnabled = true;
-              FlowControlEnabled = false;
-            };
-          };
-        };
-
-        application.extraConfig = somasis.extraConfig // {
-          General = {
-            Environment = concatStringsSep "," (
-              mapAttrsToList
-                (
-                  n: v:
-                  assert isValidPosixName n;
-                  "${n}=${v}"
-                )
-                {
-                  TERM = "xterm-256color";
-                  COLORTERM = "truecolor";
-                  EDITOR = "kate -b";
-                }
-            );
-
-            Icon = "window";
-
-            LocalTabTitleFormat = "%n";
-          };
-
-          "Interaction Options" = {
-            ColorFilterEnabled = false;
-            CtrlRequiredForDrag = false;
-            MiddleClickPasteMode = 1;
-            OpenLinksByDirectClickEnabled = false;
-            UnderlineFilesEnabled = true;
-          };
-
-          # Disable all scrolling related enhancements
-          Scrolling = {
-            HighlightScrolledLines = false;
-            HistoryMode = 1; # Limited history
-            ScrollBarPosition = 2; # Disable scrollbar
           };
         };
       };
@@ -142,7 +146,9 @@
       # Disable flow control keybinds, but only on a graphical terminal.
       # Flow control should be only controlled with Ctrl+S and Ctrl+Q on a vt,
       # so it can't be accidentally engaged without is being fixable that way.
-      [[ -v DISPLAY ]] && PROMPT_COMMAND="''${PROMPT_COMMAND:+$PROMPT_COMMAND; }stty -ixon"
+      if [[ -v DISPLAY ]] || [[ -v WAYLAND_DISPLAY ]]; then
+          PROMPT_COMMAND="''${PROMPT_COMMAND:+$PROMPT_COMMAND; }stty -ixon"
+      fi
     '';
   };
 }
