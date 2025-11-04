7f7e575801d62cddaa01ba05a360f7b6b942202f
Kylie McClain <kylie@somas.is>
2025-11-03 23:05:08 -0500

    boot: allow emergency access for failed boot

--- modules/nixos/sensible-defaults/boot.nix
+++ modules/nixos/sensible-defaults/boot.nix
@@ -1,13 +1,16 @@
 {
   config,
-  pkgs,
-  lib,
   ...
 }:
 {
-  # Allow for avoiding usage of mountpoint=legacy for the root zpool.
-  boot.zfs = {
-    extraPools = [ config.networking.fqdnOrHostName ];
-    devNodes = "/dev/disk/by-id";
+  boot = {
+    # Allow emergency access if boot fails.
+    initrd.systemd.emergencyAccess = true;
+
+    # Allow for avoiding usage of mountpoint=legacy for the root zpool.
+    zfs = {
+      extraPools = [ config.networking.fqdnOrHostName ];
+      devNodes = "/dev/disk/by-id";
+    };
   };
 }
