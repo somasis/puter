0c2b1596aed54b8d6c8d6e71e6d414e03b2a746b
Kylie McClain <kylie@somas.is>
2025-11-05 12:59:13 -0500

    console: remove color preferences

--- hosts/ilo.somas.is/console.nix
+++ hosts/ilo.somas.is/console.nix
@@ -1,29 +1,4 @@
 {
-  config,
-  lib,
-  ...
-}:
-{
-  console.colors =
-    with config.home-manager.users.somasis.theme.colors;
-    map (lib.removePrefix "#") [
-      color0
-      color1
-      color2
-      color3
-      color4
-      color5
-      color6
-      color7
-      color8
-      color9
-      color10
-      color11
-      color12
-      color13
-      color14
-      color15
-    ];
 
   # Only create two virtual terminals, one for Xorg and one for getty.
   services.logind.settings.Login.NAutoVTs = 2;
