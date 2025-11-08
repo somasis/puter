716fe28332b8d862ecafd9e11b8e7dc8e50a9e75
Kylie McClain <kylie@somas.is>
2025-11-07 23:51:52 -0500

    Move fwupd config to ilo

--- hosts/ilo.somas.is/default.nix
+++ hosts/ilo.somas.is/default.nix
@@ -105,18 +105,14 @@
       "/share/applications"
     ];
 
-  # nix = {
-  #   distributedBuilds = true;
-  #   buildMachines = [
-  #     {
-  #       hostName = "esther.7596ff.com";
-  #       sshUser = "somasis";
-  #       protocol = "ssh-ng";
-  #       system = "x86_64-linux";
-  #       speedFactor = 2;
-  #     }
-  #   ];
-  # };
+  # Keep system firmware up to date.
+  # TODO: Framework still doesn't have their updates in LVFS properly,
+  #       <https://knowledgebase.frame.work/en_us/framework-laptop-bios-releases-S1dMQt6F#:~:text=Updating%20via%20LVFS%20is%20available%20in%20the%20testing%20channel>
+  services.fwupd = {
+    enable = true;
+    extraRemotes = [ "lvfs-testing" ];
+    uefiCapsuleSettings.DisableCapsuleUpdateOnDisk = true;
+  };
 
   home-manager = {
     verbose = true;
--- modules/nixos/sensible-defaults/default.nix
+++ modules/nixos/sensible-defaults/default.nix
@@ -32,19 +32,7 @@
     self.overlays.nixpkgsVersions
   ];
 
-  i18n.extraLocales = [
-    "tok/UTF-8" # toki pona
-  ];
   console.earlySetup = true;
-
-  # Keep system firmware up to date.
-  # TODO: Framework still doesn't have their updates in LVFS properly,
-  #       <https://knowledgebase.frame.work/en_us/framework-laptop-bios-releases-S1dMQt6F#:~:text=Updating%20via%20LVFS%20is%20available%20in%20the%20testing%20channel>
-  services.fwupd = {
-    enable = true;
-    extraRemotes = [ "lvfs-testing" ];
-    uefiCapsuleSettings.DisableCapsuleUpdateOnDisk = true;
-  };
 
   persist.directories = [
     "/var/log/lastlog"
