7b594593a50d0877ee33b8f928e149556e32e9bd
Kylie McClain <kylie@somas.is>
2025-11-09 21:08:03 -0500

    ilo: remove old fwupd configuration

--- hosts/ilo.somas.is/default.nix
+++ hosts/ilo.somas.is/default.nix
@@ -98,12 +98,9 @@
       "/share/applications"
     ];
 
-  # Keep system firmware up to date.
-  # TODO: Framework still doesn't have their updates in LVFS properly,
-  #       <https://knowledgebase.frame.work/en_us/framework-laptop-bios-releases-S1dMQt6F#:~:text=Updating%20via%20LVFS%20is%20available%20in%20the%20testing%20channel>
+# Keep system firmware up to date.
   services.fwupd = {
     enable = true;
-    extraRemotes = [ "lvfs-testing" ];
     uefiCapsuleSettings.DisableCapsuleUpdateOnDisk = true;
   };
 
