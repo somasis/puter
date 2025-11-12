78de8f20d6c058f1ac6642b47a0407d40bfe5fc5
Kylie McClain <kylie@somas.is>
2025-11-12 14:40:46 -0500

    self-update: persist services.self-deploy working directory

--- modules/nixos/defaults/self-update.nix
+++ modules/nixos/defaults/self-update.nix
@@ -6,4 +6,6 @@
     repository = lib.mkDefault "https://github.com/somasis/puter.git";
     nixAttribute = lib.mkDefault "nixosConfigurations.${config.networking.hostName}";
   };
+
+  cache.directories = [ "/var/lib/nixos-self-deploy" ];
 }
