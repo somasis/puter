7854736a8c431a9d8fc26f5750088964f3ec2117
Kylie McClain <kylie@somas.is>
2025-11-18 01:30:30 -0500

    self-update: set default branch correctly

--- modules/nixos/defaults/self-update.nix
+++ modules/nixos/defaults/self-update.nix
@@ -4,6 +4,7 @@
     enable = lib.mkDefault true;
     startAt = lib.mkDefault "weekly";
     repository = lib.mkDefault "https://github.com/somasis/puter.git";
+    branch = lib.mkDefault "main";
     nixAttribute = lib.mkDefault "nixosConfigurations.${config.networking.hostName}";
   };
 }
