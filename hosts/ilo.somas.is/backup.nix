176bf1f7c0e2953051714de4e5c68e5f7d362ae2
Kylie McClain <kylie@somas.is>
2025-12-06 14:34:14 -0500

    ilo/backup: remove dead code

--- hosts/ilo.somas.is/backup.nix
+++ hosts/ilo.somas.is/backup.nix
@@ -4,43 +4,7 @@
   ...
 }:
 {
-  # services.borgbackup.jobs.persist = defaults // {
-  #   repo = repoEsther;
-  # };
-
-  # systemd = {
-  #   timers."borgbackup-job-esther".wants = [ "network-online.target" ];
-  #   services."borgbackup-job-esther" = {
-  #     unitConfig.ConditionACPower = true;
-  #     serviceConfig.Nice = 19;
-  #   };
-  # };
-
-  # environment.systemPackages =
-  #   let
-  #     borgJobs = builtins.attrNames config.services.borgbackup.jobs;
-  #     defaultArgs = lib.cli.toGNUCommandLineShell { } {
-  #       progress = true;
-  #       verbose = true;
-  #       lock-wait = 600;
-  #     };
-  #   in
-  #   [
-  #     (pkgs.borg-takeout.override {
-  #       borgConfig = config.services.borgbackup.jobs.persist;
-  #     })
-  #   ]
-  #   ++ (lib.optional (builtins.length borgJobs == 1) (
-  #     pkgs.writeShellScriptBin "borg" ''
-  #       exec borg-job-${lib.escapeShellArg (builtins.elemAt borgJobs 0)} ${defaultArgs} "$@"
-  #     ''
-  #   ));
-
   cache.directories = [
-    # {
-    #   directory = "/root/.cache/borg";
-    #   mode = "0770";
-    # }
     {
       directory = "/var/cache/restic-backups-ilo";
       mode = "0770";
