24a61ad802368aabd1f52d373a71113d954408d0
Kylie McClain <kylie@somas.is>
2025-12-08 00:42:29 -0500

    self-update: go back to using system.autoUpgrade

--- modules/nixos/defaults/self-update.nix
+++ modules/nixos/defaults/self-update.nix
@@ -1,10 +1,43 @@
-{ config, lib, ... }:
 {
-  services.self-deploy = {
-    enable = lib.mkDefault true;
-    startAt = lib.mkDefault "weekly";
-    repository = lib.mkDefault "https://github.com/somasis/puter.git";
-    branch = lib.mkDefault "main";
-    nixAttribute = lib.mkDefault "nixosConfigurations.${config.networking.hostName}";
+  config,
+  pkgs,
+  lib,
+  ...
+}:
+let
+  url = "https://github.com/somasis/puter.git";
+  clonedRepo = "/var/lib/nixos-auto-upgrade-repo";
+in
+{
+  systemd.services.nixos-upgrade.serviceConfig = {
+    ExecStartPre = pkgs.writeShellScript "nixos-upgrade-repo" ''
+      ${lib.toShellVar "url" url}
+      ${lib.toShellVar "clone" clonedRepo}
+
+      if [ -e "$clone"/.git ]; then
+          git -C "$clone" reset --hard
+          git -C "$clone" pull
+      else
+          mkdir -p "$clone"
+          git clone --depth=1 "$url" "$clone"
+      fi
+    '';
+    ExecStartPost = pkgs.writeShellScript "nixos-upgrade-repo-gc" ''
+      git -C ${clonedRepo} gc --prune=all
+    '';
   };
+
+  system.autoUpgrade = {
+    enable = lib.mkDefault true;
+    dates = lib.mkDefault "weekly";
+    flags = lib.mkDefault [
+      "--no-flake"
+      "-f"
+      clonedRepo
+      "-A"
+      "nixosConfigurations.${config.networking.hostName}"
+    ];
+  };
+
+  cache.directories = [ clonedRepo ];
 }
