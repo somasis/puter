46126c99afcb153f73d2b703feeb97c14d41bed9
Kylie McClain <kylie@somas.is>
2026-05-03 18:23:01 -0400

    defaults/boot: turn on emergencyAccess using mkDefault

--- modules/nixos/defaults/boot.nix
+++ modules/nixos/defaults/boot.nix
@@ -1,11 +1,12 @@
 {
   config,
+  lib,
   ...
 }:
 {
   boot = {
     # Allow emergency access if boot fails.
-    initrd.systemd.emergencyAccess = true;
+    initrd.systemd.emergencyAccess = lib.mkDefault true;
 
     # Allow for avoiding usage of mountpoint=legacy for the root zpool.
     zfs = {
