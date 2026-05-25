4d8ae64d7466838c460b01a6a467d8c8f385b8e6
Kylie McClain <kylie@somas.is>
2026-05-25 11:44:53 -0400

    boot: remove fun greeting line

--- hosts/ilo.somas.is/boot.nix
+++ hosts/ilo.somas.is/boot.nix
@@ -36,8 +36,6 @@
     };
   };
 
-  services.getty.greetingLine = "o kama pona tawa ${config.networking.fqdnOrHostName}.";
-
   # FIXME Remove when <https://github.com/NixOS/nixpkgs/issues/369376> is fixed
   # Workaround systemd possibly causing freezes during suspend.
   systemd.services.systemd-suspend.environment.SYSTEMD_SLEEP_FREEZE_USER_SESSIONS = "false";
