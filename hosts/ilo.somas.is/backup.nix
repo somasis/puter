2a5309bd9a518d3ead7c08d5c12ded3581af7f37
Kylie McClain <kylie@somas.is>
2026-05-25 11:35:31 -0400

    backups: split backup and check/prune jobs

--- hosts/ilo.somas.is/backup.nix
+++ hosts/ilo.somas.is/backup.nix
@@ -9,49 +9,62 @@
     restic-rclone-whatbox.file = "${self}/secrets/restic-rclone-whatbox.age";
   };
 
-  services.restic.backups.ilo = {
-    repository = "rclone:whatbox:backups/restic/ilo";
-    passwordFile = config.age.secrets.restic.path;
-    rcloneConfigFile = config.age.secrets.restic-rclone-whatbox.path;
-    initialize = true;
+  # Split backup and prune/check operations into separate ones since prune/check
+  # is more resource intensive than backups.
+  services.restic.backups = rec {
+    ilo = {
+      repository = "rclone:whatbox:backups/restic/ilo";
+      passwordFile = config.age.secrets.restic.path;
+      rcloneConfigFile = config.age.secrets.restic-rclone-whatbox.path;
+      initialize = true;
 
-    pruneOpts = [
-      "--keep-within 1h"
-      "--keep-within-hourly 12h"
-      "--keep-daily 7"
-      "--keep-weekly 4"
-      "--keep-monthly 6"
-      "--keep-yearly 1"
-      "--repack-cacheable-only"
-    ];
+      timerConfig.OnCalendar = [
+        "*:0/15:00" # every 15 minutes
+        "hourly"
+        "daily"
+        "weekly"
+        "monthly"
+      ];
 
-    timerConfig.OnCalendar = [
-      "*:0/15:00" # every 15 minutes
-      "hourly"
-      "daily"
-      "weekly"
-      "monthly"
-    ];
+      paths = [
+        "/data"
+        "/data/home/kylie"
+      ];
+      exclude = [
+        "*/Steam/steamapps/*"
 
-    paths = [
-      "/data"
-      "/data/home/kylie"
-    ];
-    exclude = [
-      "*/Steam/steamapps/*"
+        "/data/home/kylie/var/cache/rclone/*"
+        "/data/home/kylie/audio/library"
+        "/data/home/kylie/audio/source"
+        "/data/home/kylie/doc/vault"
+        "/data/home/kylie/video/anime"
+        "/data/home/kylie/video/film"
+        "/data/home/kylie/video/tv"
+      ];
 
-      "/data/home/kylie/var/cache/rclone/*"
-      "/data/home/kylie/audio/library"
-      "/data/home/kylie/audio/source"
-      "/data/home/kylie/doc/vault"
-      "/data/home/kylie/video/anime"
-      "/data/home/kylie/video/film"
-      "/data/home/kylie/video/tv"
-    ];
+      extraBackupArgs = [
+        "--one-file-system"
+        "--exclude-caches"
+      ];
+    };
 
-    extraBackupArgs = [
-      "--one-file-system"
-      "--exclude-caches"
-    ];
+    ilo-maintenance = {
+      inherit (ilo) repository passwordFile rcloneConfigFile;
+
+      createWrapper = false;
+
+      pruneOpts = [
+        "--keep-within 1h"
+        "--keep-within-hourly 12h"
+        "--keep-daily 7"
+        "--keep-weekly 4"
+        "--keep-monthly 6"
+        "--keep-yearly 1"
+        "--repack-cacheable-only"
+      ];
+
+      checkOpts = [ "--with-cache" ];
+      timerConfig.OnCalendar = [ "weekly" ];
+    };
   };
 }
