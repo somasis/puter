c27a412b75ff2005dddc1b5afd54fd7af5ee8ced
Kylie McClain <kylie@somas.is>
2026-06-19 19:21:13 -0400

    security: remove debug stuff. broken

--- modules/nixos/defaults/security.nix
+++ modules/nixos/defaults/security.nix
@@ -17,27 +17,22 @@
       wheelNeedsPassword = lib.mkDefault false;
     };
 
-    polkit = {
-      # Required so that authorization logging takes effect.
-      extraArgs = [ "--debug" ];
-
-      extraConfig = ''
-        /* Log authorization checks */
-        polkit.addRule(function(action, subject) {
-            polkit.log("action=" + action);
-            polkit.log("subject=" + subject);
-        });
-      ''
-      + (lib.optionalString (!config.security.sudo.wheelNeedsPassword) ''
-        /* Don't require a password prompt if the user is in wheel,
-         * since they could just bypass it with `sudo` anyway. */
-        polkit.addRule(function(action, subject) {
-            if (subject.isInGroup("wheel")) {
-                return polkit.Result.YES;
-            }
-        });
-      '');
-    };
+    polkit.extraConfig = ''
+      /* Log authorization checks */
+      polkit.addRule(function(action, subject) {
+          polkit.log("action=" + action);
+          polkit.log("subject=" + subject);
+      });
+    ''
+    + (lib.optionalString (!config.security.sudo.wheelNeedsPassword) ''
+      /* Don't require a password prompt if the user is in wheel,
+       * since they could just bypass it with `sudo` anyway. */
+      polkit.addRule(function(action, subject) {
+          if (subject.isInGroup("wheel")) {
+              return polkit.Result.YES;
+          }
+      });
+    '');
   };
 
   # Add the SSH keys of all users in the wheel group to the initrd
