d84718eeb34c5d119bf25872c34ba119be766dee
Kylie McClain <kylie@somas.is>
2026-07-01 19:07:57 -0400

    browser: do not hardcode esr stuff

--- users/kylie/desktop/browser.nix
+++ users/kylie/desktop/browser.nix
@@ -20,7 +20,7 @@ in
     ];
   };
 
-  home.sessionVariables.BROWSER = "firefox-esr";
+  home.sessionVariables.BROWSER = myfirefox.meta.mainProgram;
 
   xdg.mimeApps.defaultApplications = lib.genAttrs [
     "application/xhtml"
@@ -30,17 +30,20 @@ in
     "x-scheme-handler/https"
     "x-scheme-handler/about"
     "x-scheme-handler/unknown"
-  ] (_: "firefox-esr.desktop");
+  ] (_: myfirefox.desktopItem.name);
 
   home.packages =
     with pkgs;
     with kdePackages;
-    [
+    (lib.optional (myfirefox.meta.mainProgram != "firefox")
       # I think firefoxpwa can't find the default browser if it's not at `firefox`?
-      (pkgs.writeShellScriptBin "firefox" ''
-        exec firefox-esr "$@"
-      '')
-
+      (
+        pkgs.writeShellScriptBin "firefox" ''
+          exec ${lib.getExe myfirefox} "$@"
+        ''
+      )
+    )
+    ++ [
       (myfirefox.override {
         nativeMessagingHosts = [
           plasma-browser-integration
