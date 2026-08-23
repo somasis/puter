1ccc344205ef7b8d7243b6d2611b8eecbf2c9874
Kylie McClain <kylie@somas.is>
2026-08-23 15:48:06 -0400

    security: remove use of hardening profile

--- hosts/ilo.somas.is/security.nix
+++ hosts/ilo.somas.is/security.nix
@@ -13,7 +13,6 @@ let
 in
 {
   imports = [
-    "${modulesPath}/profiles/hardened.nix"
     lanzaboote.nixosModules.lanzaboote
   ];
 
@@ -41,28 +40,11 @@ in
     initrd.systemd.emergencyAccess = "$6$wdbNitvg5GyxAnQQ$bjXTIGMdmPIE0MciSAzQcLZfX0nhH72Q5PkPabB74eJkMM6mzJsR1eiG2kXjKx38dSh2swkIyeQyGdXFhqrHZ1";
   };
 
-  security = {
-    tpm2 = {
-      enable = true;
-      pkcs11.enable = true;
-      tctiEnvironment.enable = true;
-    };
-
-    # Previously disabled by hardened profile:
-    # Needed to fix builds, allegedly?
-    # <https://nixos.org/manual/nixos/unstable/#sec-profile-hardened>
-    allowUserNamespaces = true;
-    unprivilegedUsernsClone = true;
-
-    # Needed because the hardened profile affects it.
-    chromiumSuidSandbox.enable = true;
-
-    # Needed for bluetooth and wifi connectivity.
-    lockKernelModules = false;
+  security.tpm2 = {
+    enable = true;
+    pkcs11.enable = true;
+    tctiEnvironment.enable = true;
   };
-
-  # Remove when Chrome stops crashing
-  environment.memoryAllocator.provider = "libc";
 
   programs.yubikey-manager.enable = true;
   services.pcscd.enable = true;
